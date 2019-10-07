2ea3cfaa2deac365286e7d0ecc1292433060db81
francescacoo <fplacido@paypal.com>
2019-10-07 23:24:50 +0200

    added chart functions

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,12 +5,16 @@ import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 import { Platform } from '@ionic/angular';
 
 
-var event=""
+var event="";
+var arrayValue= [];
+var arrayTimes= [];
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 
 
 export class Tab1Page {
@@ -29,6 +33,8 @@ export class Tab1Page {
   lineChart: Chart;
   lineCanvas: any;
   moodValue:number; // 
+  row_data: any[];
+
 
   constructor(
     private callNumber: CallNumber,
@@ -49,7 +55,7 @@ export class Tab1Page {
     })
       .then((db: SQLiteObject) => {
         this.databaseObj = db;
-        alert('Database' + this.database_name + ' Created!');
+ //       alert('Database' + this.database_name + ' Created!');
    this.createTable();
       })
       .catch(e => {
@@ -62,24 +68,13 @@ export class Tab1Page {
     // tslint:disable-next-line:max-line-length
     this.databaseObj.executeSql('CREATE TABLE IF NOT EXISTS ' + this.table_name + ' (pid INTEGER PRIMARY KEY, mood_value INTEGER, created_at DEFAULT CURRENT_TIMESTAMP)', [])
       .then(() => {
-        alert('Table Created!');
+//        alert('Table Created!');
       })
       .catch(e => {
         alert('error ' + JSON.stringify(e));
       });
   }
 
-  insertMood() {
-    this.databaseObj.executeSql('INSERT INTO ' + this.table_name + ' (mood_value) VALUES ("' + this.moodValue+ '")', [])
-      .then(() => {
-        alert('Row mood Inserted!');
-    //    this.getPuffs();
-
-      })
-      .catch(e => {
-        alert('error ' + JSON.stringify(e))
-      });
-  }
 
   callNow(number) {
     this.callNumber.callNumber(number, true)
@@ -103,89 +98,94 @@ export class Tab1Page {
     console.log(event);
   }
 storeMood(){
-  alert("storing_mood");    
+ // alert("storing_mood");    
   this.databaseObj.executeSql('INSERT INTO ' + this.table_name + ' (mood_value) VALUES ("' + this.moodValue + '")', [])
   .then(() => {
-    alert('Row Inserted!');
-
+//    alert('Row Inserted!');
+this.addData(this.bars,"5",this.moodValue);
   })
   .catch(e => {
     alert('error ' + JSON.stringify(e));
   });                                  
 }
+
+ addData(bars, label, data) {
+  bars.data.labels.push(label);
+  bars.data.labels.splice(0,1);
+  bars.data.datasets.forEach((dataset) => {
+      dataset.data.push(data);
+      dataset.data.splice(0, 1);
+  });
+  
+  bars.update();
+}
+
+ removeData(chart) {
+  chart.datasets.data.labels.splice(0, 1);
+  chart.datasets.data.splice(0, 1);
+
+  chart.update();
+}
+
   ionViewDidEnter() {
     this.createBarChart();
-    this.createLineChart();
 
   }
 
   createBarChart() {
-    this.bars = new Chart(this.barChart.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
-        datasets: [{
-          label: 'Mood Tracker',
-          data: [25, 38, 5, 69, 91, 35, 10, 17],
-          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
-          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
+
+    this.databaseObj.executeSql("SELECT mood_value AS value, created_at AS time, pid AS id FROM " + this.table_name + " ORDER BY pid DESC LIMIT 10", [])
+    .then((res) => {
+      this.row_data = [];
+   
+      if (res.rows.length > 0) {
+        for (var i = 0; i < res.rows.length; i++) {
+        //  this.row_data.push(res.rows.item(i));
+  //      alert(res.rows.item(i).value);
+  //      this.arrayValue[i]=res.rows.item(i).value;
+        arrayValue.push(res.rows.item(i).value);
+
+        arrayTimes.push(res.rows.item(i).id);
+        }
+        this.bars = new Chart(this.barChart.nativeElement, {
+          type: 'bar',
+          data: {
+            labels: arrayTimes,
+            datasets: [{
+              label: 'Mood Score',
+              data: arrayValue,
+              backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+              borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }],
+              xAxes: [{
+                display: false //this will remove all the x-axis grid lines
+            }]
             }
-          }]
-        }
+          }
+        });
       }
+    })
+    .catch(e => {
+      alert('error boh' + JSON.stringify(e));
     });
+
+  //    alert(arrayTimes);
+
+
+
+   
   }
 
 
-
-  createLineChart() {
-  this.lineChart = new Chart(this.lineCanvas.nativeElement, {
-    type: "line",
-    data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-          label: "My First dataset",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40],
-          spanGaps: false
-        }
-      ]
-    }
-  });
-
-  }
-
-
-
-
-
-
-}
   
    
-
+}
